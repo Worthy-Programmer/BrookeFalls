875c93056bdd44c1d72421697bd6d6ae6957be59
Logan Bennett <bennett.j.logan@gmail.com>
2021-10-14 14:51:45 -0400

    Test for prod, change filename in index.json

--- src/routes/index.json.js
+++ src/routes/index.json.js
@@ -3,21 +3,25 @@ import fs from 'fs';
 import dayjs from 'dayjs';
 
 export function get() {
-  let posts = fs.readdirSync(`src/posts`)
-      .filter(fileName => /.+\.md$/.test(fileName))
-      .map(fileName => {
-        const { metadata } = process(`src/posts/${fileName}`);
-        return {
-          metadata,
-          slug: fileName.slice(0, -3)
-        };
-      });
-  // sort the posts by create date.
-  posts.sort((a, b) => (dayjs(a.metadata.date, "MMM D, YYYY") -
-                        dayjs(b.metadata.date, "MMM D, YYYY")));
-  let body = JSON.stringify(posts);
+	let posts = fs
+		.readdirSync(`/src/posts`)
+		.filter((fileName) => /.+\.md$/.test(fileName))
+		.map((fileName) => {
+			const { metadata } = process(`/src/posts/${fileName}`);
+			return {
+				metadata,
+				slug: fileName.slice(0, -3),
+			};
+		});
+	// sort the posts by create date.
+	posts.sort(
+		(a, b) =>
+			dayjs(a.metadata.date, 'MMM D, YYYY') -
+			dayjs(b.metadata.date, 'MMM D, YYYY')
+	);
+	let body = JSON.stringify(posts);
 
-  return {
-    body
-  }
+	return {
+		body,
+	};
 }
