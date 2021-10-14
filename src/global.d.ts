90de9e6396d6f1172a03c8363fadaae56d13259d
Logan Bennett <bennett.j.logan@gmail.com>
2021-10-14 16:34:56 -0400

    Misc Changes for hosting :(

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,3 @@
-/// <reference types="@sveltejs/kit" />
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
+// <reference types="@sveltejs/kit" />
+// <reference types="svelte" />
+// <reference types="vite/client" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,6 @@
       props: { posts }
     }
   }
-
 </script>
 
 <script>
