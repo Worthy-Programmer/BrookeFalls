71282c12b0f582b21cc9437c422f88c39251df88
Logan Bennett <bennett.j.logan@gmail.com>
2021-10-19 11:02:57 -0400

    Spotify Netlify Module

--- lambda/posts/posts.js
+++ lambda/posts/posts.js
@@ -1,30 +1,29 @@
-const path = require("path");
+import path from 'path';
 
-const { processFile, default: processDir } = require("md-yaml-json");
+import { processFile, processDir } from 'md-yaml-json';
 
-const postsDir = "posts/";
+const postsDir = 'posts/';
 const absPostsDir = path.join(__dirname, postsDir);
-
 
 exports.handler = async (event) => {
   const { name } = event.queryStringParameters;
 
-    let body = [];
+  let body = [];
 
-    /* If parameter "name" is given, return only the specific markdown file
+  /* If parameter "name" is given, return only the specific markdown file
     else return the details of all the markdown files */
 
-    if (name) {
-      body = processFile(absPostsDir + name + ".md");
-    } else {
-      body = processDir(absPostsDir);
-    }
+  if (name) {
+    body = processFile(absPostsDir + name + '.md');
+  } else {
+    body = processDir(absPostsDir);
+  }
 
-    return {
-      body: JSON.stringify(body),
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
+  return {
+    body: JSON.stringify(body),
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 };
