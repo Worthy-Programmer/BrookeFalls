e6231702c1168bf819081e57056bb91a7625c46a
Logan Bennett <bennett.j.logan@gmail.com>
2021-10-19 11:13:30 -0400

    Spotify Netlify Lambda Changes

--- src/envFunctions.js
+++ src/envFunctions.js
@@ -1,8 +1,8 @@
-export function getSession() {
+exports.getSession = () => {
   return {
     CLIENT_ID: import.meta.env.VITE_CLIENT_ID,
     CLIENT_SECRET: import.meta.env.VITE_CLIENT_SECRET,
     REDIRECT_URL: import.meta.env.VITE_REDIRECT_URL,
     AUTH_CODE: import.meta.env.VITE_AUTH_CODE,
   };
-}
+};
